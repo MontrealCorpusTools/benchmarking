a7152f615162642d841263488a1f73a0dbd05783
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-05-18 15:58:55 -0400

    Add mandarin character model

--- MFA/benchmark_g2p_globalphone_all.py
+++ MFA/benchmark_g2p_globalphone_all.py
@@ -98,13 +98,16 @@ def g2p_gp(lang_code, full_name):
         t = PhonetisaurusTrainer(dictionary, output_model_path, temp_directory=temp_directory, window_size=best_size)
         t.train()
 
-    if lang_code in  ['FR', 'GE']:
+    if lang_code in  ['FR', 'GE', 'CH']:
         if lang_code == 'FR':
             dictionary_path = '/media/share/corpora/GP_for_MFA/{0}/dict/fr.dict'.format(lang_code)
             output_model_path = '/data/mmcauliffe/aligner-models/g2p/{}_prosodylab_g2p.zip'.format(full_name)
         elif lang_code == 'GE':
             dictionary_path = '/media/share/corpora/GP_for_MFA/{0}/dict/de.dict'.format(lang_code)
             output_model_path = '/data/mmcauliffe/aligner-models/g2p/{}_prosodylab_g2p.zip'.format(full_name)
+        elif lang_code == 'CH':
+            dictionary_path = '/media/share/corpora/GP_for_MFA/{0}/dict/char_dict.txt'.format(lang_code)
+            output_model_path = '/data/mmcauliffe/aligner-models/g2p/{}_character_g2p.zip'.format(full_name)
         if not os.path.exists(dictionary_path):
             print('Skipping {}, no dictionary!'.format(lang_code))
             return
